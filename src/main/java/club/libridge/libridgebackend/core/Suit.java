d4f92ed36659b200a7d7c79cabd52e0ec02a1e67
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2024-08-13 03:58:45 -0300

    [refactor] Card to scala

--- src/main/java/club/libridge/libridgebackend/core/Suit.java
+++ src/main/java/club/libridge/libridgebackend/core/Suit.java
@@ -2,9 +2,7 @@ package club.libridge.libridgebackend.core;
 
 import club.libridge.libridgebackend.core.exceptions.SuitDoesNotExistException;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public enum Suit {
     DIAMONDS("Diamonds", "d", '\u2666'), CLUBS("Clubs", "c", '\u2663'), HEARTS("Hearts", "h", '\u2665'), SPADES("Spades", "s", '\u2660');
@@ -26,4 +24,15 @@ public enum Suit {
         throw new SuitDoesNotExistException();
     }
 
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public char getUnicodeSymbol() {
+        return this.unicodeSymbol;
+    }
 }
