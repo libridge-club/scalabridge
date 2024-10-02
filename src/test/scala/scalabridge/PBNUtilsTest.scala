7ab94addcf36d426605e3de7a0db18ed0391947e
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2024-10-02 15:18:12 -0300

    [test] add one more future test to PBNUtils

--- src/test/scala/scalabridge/PBNUtilsTest.scala
+++ src/test/scala/scalabridge/PBNUtilsTest.scala
@@ -65,4 +65,26 @@ class PBNUtilsTest extends UnitFunSpec with EitherValues { // FIXME should uncom
   //     }
   //   }
   // }
+
+  // This one was from HandBuilder
+  // @Test
+  //   public void myTest() {
+  //       String first = "q5.kt85.qjt8632.";
+  //       HandBuilder handBuilder = new HandBuilder();
+  //       Card sq = new Card(Suit.getSPADES(), Rank.getQUEEN());
+  //       Card s5 = new Card(Suit.getSPADES(), Rank.getFIVE());
+  //       Card s3 = new Card(Suit.getSPADES(), Rank.getTHREE());
+  //       Card ht = new Card(Suit.getHEARTS(), Rank.getTEN());
+  //       Card d8 = new Card(Suit.getDIAMONDS(), Rank.getEIGHT());
+  //       Card ca = new Card(Suit.getCLUBS(), Rank.getACE());
+
+  //       Hand firstHand = handBuilder.buildFromDotSeparatedString(first);
+
+  //       assertTrue(firstHand.containsCard(sq));
+  //       assertTrue(firstHand.containsCard(s5));
+  //       assertFalse(firstHand.containsCard(s3));
+  //       assertTrue(firstHand.containsCard(ht));
+  //       assertTrue(firstHand.containsCard(d8));
+  //       assertFalse(firstHand.containsCard(ca));
+  //   }
 }
